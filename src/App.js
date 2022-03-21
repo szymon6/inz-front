23cda99c13cd1f23b24f252f4362b63b2d77ebf1
name <you@example.com>
2022-03-21 11:49:23 +0100

    c

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import Frame from './components/Frame'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import Table from './pages/Dashboard/Table'
+import Test from './pages/Dashboard/Test'
+import LoginPage from './pages/LoginPage'
 
 function App() {
   return (
     <div className="App">
-      <Frame />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard />}>
+            <Route path="table" element={<Table />} />
+            <Route path="test" element={<Test />} />
+          </Route>
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
 
 export default App
+
+//przykładowe linki
+
+// .com/Dashboard/Table/JakasTabela
+//// .com/Dashboard/Table/JakasTabela/1
+//.com/Login
---
+++ src/pages/Dashboard/Table.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Table = () => {
+  return <div>Tabela</div>
+}
+
+export default Table
