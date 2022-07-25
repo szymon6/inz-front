85bc094e5fb6da5e45f023ef1d3641cd05482116
szymon6 <72827230+szymon6@users.noreply.github.com>
2022-07-25 11:20:26 +0200

    c

--- src/components/DraggableDialog.js
+++ src/components/DraggableDialog.js
@@ -2,22 +2,19 @@ import Button from '@mui/material/Button'
 import Dialog from '@mui/material/Dialog'
 import DialogActions from '@mui/material/DialogActions'
 import DialogContent from '@mui/material/DialogContent'
-import DialogTitle from '@mui/material/DialogTitle'
 import Table from './Table'
 
 export default function EditDropdownDialog({ close }) {
   return (
     <div>
       <Dialog open={true} onClose={close}>
-        <DialogTitle>Subscribe</DialogTitle>
         <DialogContent>
           <Table name="role" dropdown />
         </DialogContent>
         <DialogActions>
           <Button autoFocus onClick={close}>
-            Cancel
+            Back
           </Button>
-          <Button onClick={close}>Subscribe</Button>
         </DialogActions>
       </Dialog>
     </div>
