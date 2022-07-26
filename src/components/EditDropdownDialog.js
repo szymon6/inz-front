98aeac2da4348f91aebb32cc857ce482f9843057
szymon6 <72827230+szymon6@users.noreply.github.com>
2022-07-26 16:28:14 +0200

    c

--- src/components/EditDropdownDialog.js
+++ src/components/EditDropdownDialog.js
@@ -1,14 +1,30 @@
-import Button from '@mui/material/Button'
-import Dialog from '@mui/material/Dialog'
-import DialogActions from '@mui/material/DialogActions'
-import DialogContent from '@mui/material/DialogContent'
-import Table from './Table'
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import TextField from '@mui/material/TextField';
+import Table from './Table';
 
 export default function EditDropdownDialog({ dropdown, close }) {
   return (
     <div>
       <Dialog fullWidth maxWidth="xs" open={true} onClose={close}>
         <DialogContent>
+          <form
+            style={{
+              marginBottom: 20,
+              display: 'flex',
+            }}
+          >
+            <TextField
+              label="New Value"
+              variant="standard"
+              sx={{ width: '100%' }}
+            />
+            <Button variant="contained" color="primary">
+              Add
+            </Button>
+          </form>
           <Table name={dropdown} dropdown />
         </DialogContent>
         <DialogActions>
@@ -18,5 +34,5 @@ export default function EditDropdownDialog({ dropdown, close }) {
         </DialogActions>
       </Dialog>
     </div>
-  )
+  );
 }
