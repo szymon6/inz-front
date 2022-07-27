cdc0e6f9bf3184bd309deba8d8f4197f8413986d
szymon6 <72827230+szymon6@users.noreply.github.com>
2022-07-27 17:14:48 +0200

    c

--- src/components/EditDropdownDialog.js
+++ src/components/EditDropdownDialog.js
@@ -1,16 +1,34 @@
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import TextField from '@mui/material/TextField';
-import Table from './Table';
+import Button from '@mui/material/Button'
+import Dialog from '@mui/material/Dialog'
+import DialogActions from '@mui/material/DialogActions'
+import DialogContent from '@mui/material/DialogContent'
+import TextField from '@mui/material/TextField'
+import { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import api from '../api'
+import Table from './Table'
 
 export default function EditDropdownDialog({ dropdown, close }) {
+  const { register, handleSubmit, reset } = useForm()
+  const [reload, setReload] = useState(false)
+
+  const submit = async ({ value }) => {
+    console.log(dropdown)
+    await api.post(`table/${dropdown}`, { value })
+    setReload(true)
+    reset()
+  }
+
+  useEffect(() => {
+    setReload(false)
+  }, [reload])
+
   return (
     <div>
       <Dialog fullWidth maxWidth="xs" open={true} onClose={close}>
         <DialogContent>
           <form
+            onSubmit={handleSubmit(submit)}
             style={{
               marginBottom: 20,
               display: 'flex',
@@ -20,12 +38,13 @@ export default function EditDropdownDialog({ dropdown, close }) {
               label="New Value"
               variant="standard"
               sx={{ width: '100%' }}
+              {...register('value')}
             />
-            <Button variant="contained" color="primary">
+            <Button type="submit" variant="contained" color="primary">
               Add
             </Button>
           </form>
-          <Table name={dropdown} dropdown />
+          {!reload && <Table name={dropdown} dropdown />}
         </DialogContent>
         <DialogActions>
           <Button autoFocus onClick={close}>
@@ -34,5 +53,5 @@ export default function EditDropdownDialog({ dropdown, close }) {
         </DialogActions>
       </Dialog>
     </div>
-  );
+  )
 }
